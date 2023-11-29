20604c80412c68c8b765fca63420db9645afde77
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-11-29 17:07:02 +0200

    fixes abbreviation of initialization

--- src/2.ts
+++ src/2.ts
@@ -41,12 +41,8 @@ class Employee {
   constructor(
     public name: string,
     private department: string,
-    protected salary: number,
-  ) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
